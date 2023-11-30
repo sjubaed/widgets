9daffb3805745d3c7b3f4370255ab5fbc12e7cb5
Sam Jubaed <samiuljubaed@gmail.com>
2023-11-30 15:01:06 -0500

    add axes

--- d3-react/src/runtime/widget.tsx
+++ d3-react/src/runtime/widget.tsx
@@ -20,27 +20,113 @@
 */
 import { React, jsx } from 'jimu-core'
 import * as d3 from 'd3'
-const { useEffect, useRef } = React
+const { useEffect, useRef, useState, useMemo } = React
 
-export default function Widget {
+// interface Data {
+//   label: string;
+//   value: number;
+// }
 
-  const Circle = () => {
-    const ref = useRef()
+// const DATA: Data[] = [
+//   { label: "Apples", value: 100 },
+//   { label: "Bananas", value: 200 },
+//   { label: "Oranges", value: 50 },
+//   { label: "Kiwis", value: 150 }
+// ];
 
-    useEffect(() => {
-      const svgElement = d3.select(ref.current)
-      svgElement.append("circle")
-        .attr("cx", 150)
-        .attr("cy", 70)
-        .attr("r", 50)
-    }, [])
-
-    return (
-      <svg
-      ref={ref}
-      />
-      )
-  }
-
-
+interface AxisBottomProps {
+  scale: ScaleBand<string>;
+  transform: string;
 }
+
+interface AxisLeftProps {
+  scale: ScaleLinear<number, number, never>;
+}
+
+interface BarsProps {
+  data: BarChartProps["data"];
+  height: number;
+  scaleX: AxisBottomProps["scale"];
+  scaleY: AxisLeftProps["scale"];
+}
+
+// interface BarChartProps {
+//   data: Data[];
+// }
+
+const data2 = [100, 200, 50, 150]
+
+
+function barChart({ data }: BarChartProps) {
+  const margin = { top: 10, right: 0, bottom: 20, left: 30 };
+  const width = 500 - margin.left - margin.right;
+  const height = 300 - margin.top - margin.bottom;
+
+  const scaleX = d3.scaleBand(["Apples", "Bananas", "Oranges", "Kiwis"], [0, width]);
+  const scaleY = d3.scaleLinear([200,0], [0, height]);
+  console.log(data2)
+
+  return (
+    <svg
+      width={width + margin.left + margin.right}
+      height={height + margin.top + margin.bottom}
+    >
+      <g transform={`translate(${margin.left}, ${margin.top})`}>
+        <AxisBottom scale={scaleX} transform={`translate(0, ${height})`} />
+        <AxisLeft scale={scaleY} />
+        <Bars data={data2} />
+      </g>
+    </svg>
+  );
+}
+
+function AxisBottom({ scale, transform }: AxisBottomProps) {
+  const ref = useRef<SVGGElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      d3.select(ref.current).call(d3.axisBottom(scale));
+    }
+  }, [scale]);
+
+  return <g ref={ref} transform={transform} />;
+}
+
+function AxisLeft({ scale }: AxisLeftProps) {
+  const ref = useRef<SVGGElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      d3.select(ref.current).call(d3.axisLeft(scale));
+    }
+  }, [scale]);
+
+  return <g ref={ref} />;
+}
+
+function Bars({ data }: BarsProps) {
+  const ref = useRef<SVGGElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      const bar = d3.select("bar");
+      const barUpdate = bar.data(data);
+      const barNew = barUpdate.join("bar")
+      barNew.style('background', 'steelblue')
+      // barNew.style('padding', '3px')
+      // barNew.style('margin', '4px')
+      // barNew.style('width', (d) => `${x(d)}px`)
+      // barNew.text((d) => d)
+
+      //ref.current.appendChild(svg.node())
+
+
+
+
+    }
+  }, [data])
+
+  return <g ref={ref} />;
+}
+
+export default barChart
